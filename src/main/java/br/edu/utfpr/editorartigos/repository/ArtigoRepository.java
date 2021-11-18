abaa6057029e254b73b7d61fbd46e6803f1e581b
Diego Beutler <diegobeutler2411@gmail.com>
2021-11-18 18:36:37 -0300

    Removido código desnecessário

--- src/main/java/br/edu/utfpr/editorartigos/repository/ArtigoRepository.java
+++ src/main/java/br/edu/utfpr/editorartigos/repository/ArtigoRepository.java
@@ -4,6 +4,5 @@ import br.edu.utfpr.editorartigos.model.Artigo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ArtigoRepository extends JpaRepository<Artigo, Long> {
 }
