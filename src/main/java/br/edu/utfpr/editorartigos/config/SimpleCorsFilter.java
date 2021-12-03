6caf4c8b365ecdb3902525051b30b2cde176f78c
Diego Beutler <diegobeutler2411@gmail.com>
2021-12-02 22:21:13 -0300

    Ajustado Cors

--- src/main/java/br/edu/utfpr/editorartigos/config/SimpleCorsFilter.java
+++ src/main/java/br/edu/utfpr/editorartigos/config/SimpleCorsFilter.java
@@ -1,6 +1,5 @@
 package br.edu.utfpr.editorartigos.config;
 
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Order(HIGHEST_PRECEDENCE)
 public class SimpleCorsFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest req,
-                         ServletResponse res,
-                         FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -26,21 +25,18 @@ public class SimpleCorsFilter implements Filter {
                 + "Content-Type, Accept, X-CSRF-TOKEN, Cache-Control, DNT, X-CustomHeader, Keep-Alive, "
                 + "User-Agent, If-Modified-Since, Content-Range, Range");
         response.setHeader("Access-Control-Max-Age", "3600");
-
-        if("OPTION".equalsIgnoreCase(((HttpServletRequest) req).getMethod())){
+        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
-            filterChain.doFilter(req, res);
+            chain.doFilter(req, res);
         }
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+    public void destroy() {
     }
 
     @Override
-    public void destroy() {
-        Filter.super.destroy();
+    public void init(FilterConfig config) throws ServletException {
     }
 }
